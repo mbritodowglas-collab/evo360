695e592a9f332a3488cb5d2d77e3142bc60be614
mbritodowglas-collab <mbritodowglas@gmail.com>
2025-10-19 13:32:36 -0400

    Atualizar o nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,7 +2,6 @@ function navTo(page) {
   window.location.href = page;
 }
 
-// destaca ícone ativo no rodapé
 const current = location.pathname.split("/").pop();
 const links = document.querySelectorAll(".navbar a");
 links.forEach(link => {
